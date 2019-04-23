64beaf0433992d8b7b6aaae288763c5e26833897
Patrick R Sears <patrick_sears@med.unc.edu>
2019-04-23 09:41:12 -0400

    Check for arguments.

--- go.py
+++ go.py
@@ -2,7 +2,17 @@
 
 print()
 
+import sys
+
+
 print("This is just a test.")
+print()
+
+if len(sys.argv) > 1:
+  print("There was at least one argument.")
+  for i in range(1, len(sys.argv)):
+    print("  arg["+str(i)+"]: ", sys.argv[i])
+
 
 print()
 
