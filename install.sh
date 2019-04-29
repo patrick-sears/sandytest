2863d882cf0ce61d1f518912302ec9604cf990bd
Patrick R Sears <patrick_sears@med.unc.edu>
2019-04-29 12:08:32 -0400

    Created install system.
    It now looks for $HOME/bin.  If it finds it, it
    creates a link in there to go.py.
    
    It also installs the lib.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+progdir=`pwd -P`
+name="sandytest"
+
+if [[ -e "$HOME/bin" ]]; then
+  cd "$HOME/bin"
+  if [[ -L "$name" ]]; then
+    unlike "$name"
+  fi
+  ln -s "$progdir/go.py"
+else
+  echo "No home bin dir, so not creating any link."
+fi
+
+
+
+cd "$progdir/libs"
+./install_lib.sh
+
+
+
+
