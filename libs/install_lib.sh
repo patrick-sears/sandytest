6082343565a68d58c23c185997c4375692fac14a
Patrick R Sears <pat@mplesiosaur.nautae>
2021-05-17 19:14:13 -0400

    Add info to error message from libs/install_lib.sh

--- libs/install_lib.sh
+++ libs/install_lib.sh
@@ -5,7 +5,13 @@ d1=`python3 -m site --user-site`
 
 if ! [[ -e "$d1" ]]; then
   echo "Error.  Couldn't find d1".
-  echo; exit 1
+  echo "  d1:  $d1"
+  echo "  d1 was generated with the following command:"
+  echo "    python3 -m site --user-site"
+  echo "  Maybe you should try to create it manually and try to install again?"
+  echo "  I'm not really sure."
+  echo
+  exit 1
 fi
 
 d2="$d1/prs"
