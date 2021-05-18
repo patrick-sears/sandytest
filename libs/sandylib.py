2033b662c9cb973440496aa20eae707055999612
Patrick R Sears <pat@pskate.med.unc.edu>
2021-05-18 07:55:48 -0400

    Add class C_sandy to sandylib

--- libs/sandylib.py
+++ libs/sandylib.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import math
+
 
 def sandylib_print(x):
   print("In sandy lib print.")
@@ -7,4 +9,15 @@ def sandylib_print(x):
   return
 
 
+############################################
+class C_sandy:
+  ###
+  def __init__(self):
+    self.x = 0
+    self.y = 0
+  ###
+  def mag2(self):
+    return self.x * self.x + self.y * self.y
+  ###
+############################################
 
